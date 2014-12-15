9f1c7db5c50cf2db1d412a03a32dd39b650b22c9
Alejandro Baez <Alejan.Baez@gmail.com>
2014-12-14 19:02:01 -0500

    updated snippets for yaml

--- modules/yaml/init.lua
+++ modules/yaml/init.lua
@@ -4,9 +4,9 @@ if type(snippets) == 'table' then
   snippets.yaml = {
     ["-"]     = "- %1(name): %0",
     ["---"]   = "---\n%1(name): %0",
-    ["f"]     = "\n\t%1(name): %0",
-    ["n"]     = "\n%1(name): %0",
-    ["{"]     = "{{ %1(name) }}%0",
+    ["f"]     = "\t%1(param): %0",
+    ["n"]     = "%1(param): %0",
+    ["i"]     = "with_items:\n\t- %1\n\t-%0",
   }
 end
 
