7de6a2abd02bd49048519032a227dfb4a8940b4f
Alejandro Baez <Alejan.Baez@gmail.com>
2015-01-04 16:16:13 -0500

    changed color order

--- properties.lua
+++ properties.lua
@@ -51,8 +51,9 @@ events.connect(events.LEXER_LOADED, function (lang)
   buffer.property['style.function'] = 'fore:%(color.base08)'
   buffer.property['style.library'] = 'fore:%(color.base09)'
   buffer.property['style.identifier'] = 'fore:%(color.base0D)'
-  buffer.property['style.number'] = 'fore:%(color.base0A)'
-  buffer.property['style.constant'] = 'fore:%(color.base0E)'
+  buffer.property['style.number'] = 'fore:%(color.base0E)'
+  buffer.property['style.constant'] = 'fore:%(color.base0A)'
+
 
   buffer.property['style.keyword'] = ct.background == '-light' and
     'fore:%(color.base02)' or 'fore:%(color.base05)'
