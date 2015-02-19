2e176844b793ff870248ab378b0a32e45230df19
Alejandro Baez <Alejan.Baez@gmail.com>
2015-02-19 00:16:17 -0500

    ordering of semantic

--- properties.lua
+++ properties.lua
@@ -50,13 +50,13 @@ events.connect(events.LEXER_LOADED, function (lang)
   if _SEMANATIC == false then return end
   if dont[lang] then return end
 
-  buffer.property['style.operator'] = 'fore:%(color.base0F)'
-  buffer.property['style.function'] = 'fore:%(color.base08)'
-  buffer.property['style.library'] = 'fore:%(color.base09)'
+  buffer.property['style.operator']   = 'fore:%(color.base0F)'
+  buffer.property['style.function']   = 'fore:%(color.base08)'
+  buffer.property['style.library']    = 'fore:%(color.base09)'
   buffer.property['style.identifier'] = 'fore:%(color.base0D)'
-  buffer.property['style.number'] = 'fore:%(color.base0E)'
-  buffer.property['style.constant'] = 'fore:%(color.base0A)'
+  buffer.property['style.number']     = 'fore:%(color.base0E)'
+  buffer.property['style.constant']   = 'fore:%(color.base0A)'
 
-  buffer.property['style.keyword'] = ct.background == '-light' and
+  buffer.property['style.keyword']    = ct.background == '-light' and
     'fore:%(color.base02)' or 'fore:%(color.base05)'
 end)
