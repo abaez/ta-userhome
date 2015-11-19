f1138a4853a355db8ab48b0b9c50c7a3667f99a8
Alejandro Baez <code@baez.nyc>
2015-11-18 22:44:04 -0500

    properties: reformat using Mitchell's properties

--- properties.lua
+++ properties.lua
@@ -8,7 +8,8 @@
 _SEMANATIC = true
 
 --- holds languages NOT to use semantic highlighting if semantic is on.
--- @param `language = true`: language being any one you choose.
+-- @table dont
+-- @field `language = true`: language being any one you choose.
 local dont = {
   perl = true,
   yaml = true,
@@ -16,26 +17,42 @@ local dont = {
   toml = true
 }
 
--- 4 spaces as a default
---buffer.tab_width = 4
---buffer.use_tabs = false
-
 -- column width settings
 buffer.edge_column = 79
 buffer.edge_mode = buffer.EDGE_LINE -- line marker
 --buffer.edge_mode = buffer.EDGE_BACKGROUND -- colors all after edge to colour
---buffer.edge_colour = "0x1132FF"
---buffer.edge_colour = "0xFF00FF"
 
--- multi select
-buffer.multiple_selection = true
-buffer.additional_selection_typing = true
-buffer.additional_carets_visible = true
-
--- scrollbar
+-- scrolling
+buffer:set_x_caret_policy(buffer.CARET_SLOP, 20)
+buffer:set_y_caret_policy(buffer.CARET_SLOP + buffer.CARET_STRICT +
+                          buffer.CARET_EVEN, 1)
+buffer.h_scroll_bar = false
+buffer.v_scroll_bar = false
 --buffer.scroll_width_tracking = true
 --buffer.scroll_width = 120
 buffer.end_at_last_line = false
+
+
+-- Multiple Selection and Virtual Space
+buffer.multiple_selection = true
+buffer.additional_selection_typing = true
+--buffer.multi_paste = buffer.MULTIPASTE_EACH
+--buffer.virtual_space_options = buffer.VS_RECTANGULARSELECTION +
+--                               buffer.VS_USERACCESSIBLE
+buffer.rectangular_selection_modifier = buffer.MOD_ALT
+buffer.mouse_selection_rectangular_switch = true
+--buffer.additional_carets_blink = false
+--buffer.additional_carets_visible = false
+
+-- Tabs and Indentation Guides.
+-- Note: tab and indentation settings apply to individual buffers.
+buffer.tab_width = 2
+buffer.use_tabs = false
+buffer.tab_indents = true
+buffer.back_space_un_indents = true
+buffer.indentation_guides = not CURSES and buffer.IV_LOOKBOTH or buffer.IV_NONE
+
+
 
 -- auto complete
 --buffer.auto_c_separator = '32' -- bytes
